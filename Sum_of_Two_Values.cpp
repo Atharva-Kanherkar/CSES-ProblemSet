5908f7d9a2553a340624076c8e1d360bebe81a81
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2023-12-11 11:44:39 +0530

    gives tle in only two cases, but still a  very relevant solution

--- Sum_of_Two_Values.cpp
+++ Sum_of_Two_Values.cpp
@@ -12,14 +12,22 @@ int main() {
     for (int i = 0; i < n; i++) {
            cin>>a[i];
     }
-    
+    pair<int, int> ans = {-1, -1};
     for(int i=0; i<n; i++){
         int c = a[i];
         int more = x - c;
         if(mp.find(more)!=mp.end()){
-             
+             ans.first = mp[more];
+             ans.second = i;
+             break;
         }
+        mp[c] = i;
     }
+    if(ans.first == -1){
+        cout<<"IMPOSSIBLE"<<endl;
+    }
+    else
+    cout<<ans.first+1<<" "<<ans.second+1<<endl;
  
 
     return 0;
