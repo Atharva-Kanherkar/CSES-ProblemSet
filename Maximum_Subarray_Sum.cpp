d592d85ed28d3cff4cbcdf880d864ec2eddad2d2
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2023-12-11 11:25:21 +0530

    max  subarray sum

--- Maximum_Subarray_Sum.cpp
+++ Maximum_Subarray_Sum.cpp
@@ -4,22 +4,24 @@ using namespace std;
 #define ll long long
 
 int main() {
-    ll n, x;
-    cin >> n >> x;
+    ll n;
+    cin >> n;
     vector<int> a(n);
-    unordered_map<int, int> mp;
-    
+     
     for (int i = 0; i < n; i++) {
            cin>>a[i];
     }
-    
+    int sum = 0;
+    int max_sum = 0;
     for(int i=0; i<n; i++){
-        int c = a[i];
-        int more = x - c;
-        if(mp.find(more)!=mp.end()){
-             
-        }
+         sum = max(sum+a[i], a[i]);
+         max_sum = max(sum, max_sum);
     }
+    cout<<max_sum<<endl;
+    
+//kadane's algorithm
+    
+ 
  
 
     return 0;
