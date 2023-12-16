6bc8e5338ccd65973837a7de9ec2f9ce94673150
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2023-12-16 11:18:50 +0530

    mcs

--- Towers.cpp
+++ Towers.cpp
@@ -14,7 +14,7 @@ int main() {
     ll ans = 1;
     sort(p.rbegin(), p.rend());
     for (int i = 0; i < n - 1; i++) {
-        if (p[i + 1].first > p[i].first || p[i + 1].second < p[i].second) {
+        if (p[i + 1].first >= p[i].first || p[i + 1].second < p[i].second) {
             ans++;
         }
     }
