bc73e1cd79ec05a0974a17cbc33dcf65e4f7b483
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2023-12-16 11:39:24 +0530

    Stick lenghts

--- Stick_Lengths.cpp
+++ Stick_Lengths.cpp
@@ -11,45 +11,20 @@ int main() {
 
     for(int i = 0; i < n; i++) {
         cin >> a[i];
-        mp[a[i]]++;
+      
     }
-    bool ok = false;
-    if(a[0]==881618352 ){
-        ok = true;
-    }
-
-    sort(a.begin(), a.end());
+  
+    sort(a.rbegin(), a.rend());
 
     
-    ll maxFreqElement = -1;
-    ll maxFreq = 0;
-
-    for(auto it = mp.begin(); it != mp.end(); it++) {
-        if(it->second > maxFreq) {
-            maxFreq = it->second;
-            maxFreqElement = it->first;
-        }
-    }
-    ll ans = 0;
-    if(maxFreq>1){
-        for(int i=0; i<n; i++){
-            ans+= abs(maxFreqElement-a[i]);
-        }
-    }
-    else{
+        ll ans =0;
         ll mid = n/2;
         ll mini = a[mid];
         for(int i=0; i<n; i++){
             ans+=abs(mini-a[i]);
         }
-    }
-    if(ok){
-        cout<<49955518418712<<endl;
-        //just cannot figure this shit out 
-
-    }
-    else
-  
+    
+ 
     cout<<ans<<endl;
    
 
